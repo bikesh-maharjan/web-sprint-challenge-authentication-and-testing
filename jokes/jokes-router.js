7dfddf397edb5d55d95871bd083a0c12604ad420
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-09-18 09:05:03 -0700

    added restricted auth with a get request

--- jokes/jokes-router.js
+++ jokes/jokes-router.js
@@ -1,19 +1,33 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const router = require('express').Router();
+const Users = require("../users/user-model");
 
-router.get('/', (req, res) => {
+const restricted = require("../auth/authenticate-middleware");
+
+const router = require("express").Router();
+
+router.get("/", restricted, (req, res) => {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" },
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
-    .then(response => {
+    .get("https://icanhazdadjoke.com/search", requestOptions)
+    .then((response) => {
       res.status(200).json(response.data.results);
     })
-    .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+    .catch((err) => {
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
+    });
+});
+
+router.get("/user", restricted, (req, res) => {
+  Users.find()
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
     });
 });
 
