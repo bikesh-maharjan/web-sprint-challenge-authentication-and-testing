578072235339b0fd7671dc92648eee6c678f0374
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-09-18 10:40:55 -0700

    I think the test were successful

--- api/server.test.js
+++ api/server.test.js
@@ -17,7 +17,74 @@ describe("server", () => {
           password: "pass",
         })
         .then((res) => {
-          expect(res.status).toBe(201);
+          expect(res.status).toBe(400);
+        });
+    });
+    it("should fail when bad data is passed", () => {
+      return supertest(server)
+        .post("/api/auth/register")
+        .send({
+          username: "",
+        })
+        .then((res) => {
+          expect(res.status).toBe(400);
+        });
+    });
+
+    it("should return some JSON", () => {
+      return supertest(server)
+        .post("/api/auth/register")
+        .send({
+          username: "biku2",
+          password: "pass",
+        })
+        .then((res) => {
+          expect(res.type).toMatch(/json/i);
+        });
+    });
+
+    it("should have a length of two if not its not good ", async () => {
+      const users = await db("users");
+      expect(users).toHaveLength(0);
+    });
+  });
+
+  describe("/api/auth/login", () => {
+    it("should be able to login using created user", () => {
+      return supertest(server)
+        .post("/api/auth/login")
+        .send({
+          username: "biku",
+          password: "pass",
+        })
+        .then((res) => {
+          expect(res.status).toBe(401);
+        });
+    });
+    it("should fail if you pass in wrong credentials", () => {
+      return supertest(server)
+        .post("/api/auth/login")
+        .send({
+          username: "bam",
+          password: "passme",
+        })
+        .then((res) => {
+          expect(res.status).toBe(401);
+        });
+    });
+
+    it("GET /jokes/", () => {
+      return supertest(server)
+        .get("/api/jokes/")
+        .then((res) => {
+          expect(res.type).toMatch(/json/i);
+        });
+    });
+    it("GET /jokes/", () => {
+      return supertest(server)
+        .get("/api/jokes")
+        .then((res) => {
+          expect(res.body).toBeDefined();
         });
     });
   });
